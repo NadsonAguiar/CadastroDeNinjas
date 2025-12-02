555093562be667f23e339352b083a9625da97a6c
Nadson Aguiar <164579532+NadsonAguiar@users.noreply.github.com>
2025-12-01 23:10:37 -0300

    fix: Adiciona o @Column na minha classe

--- src/main/java/dev/nadsonaguiar/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/nadsonaguiar/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.nadsonaguiar.CadastroDeNinjas.Ninjas;
 
 import dev.nadsonaguiar.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,18 +12,24 @@ import java.util.List;
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     // Id controla nosso atributo ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Gera id sequenciais
+    @Column (name = "id")
     private Long id;
 
+    @Column (name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column (name = "idade")
     private int idade;
 
     // @ManyToOne um ninja tem uma unica missão
@@ -28,38 +37,5 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //Foreing Key ou chave estrangeira
     private MissoesModel missoes;
 
-    // No args
-    public NinjaModel() {
-    }
 
-    // Quase allArgs
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
