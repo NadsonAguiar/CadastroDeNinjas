099370c34d3640686b67c57f70c2d5ca1515ee67
Nadson Aguiar <164579532+NadsonAguiar@users.noreply.github.com>
2025-12-07 19:22:00 -0300

    chore:Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade e DTO

--- src/main/java/dev/nadsonaguiar/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/nadsonaguiar/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,24 @@
 package dev.nadsonaguiar.CadastroDeNinjas.Ninjas;
 
+import dev.nadsonaguiar.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
+
+
+
+
 }
