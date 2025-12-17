87369c04c7fef71dec60b31ad98717a487e916b6
Nadson Aguiar <164579532+NadsonAguiar@users.noreply.github.com>
2025-12-16 22:29:49 -0300

    feat: Inclui as annotation de validação e mensagens personalizadas, para que os campos do DTO  sejam preenchidos corretamente

--- src/main/java/dev/nadsonaguiar/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/nadsonaguiar/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,9 +1,16 @@
 package dev.nadsonaguiar.CadastroDeNinjas.Ninjas;
 
 import dev.nadsonaguiar.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+
+
 
 
 @Data
@@ -12,10 +19,20 @@ import lombok.NoArgsConstructor;
 public class NinjaDTO {
 
     private Long id;
+    @NotBlank(message = "Nome é obrigatório")
     private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email deve ser válido")
     private String email;
-    private int idade;
+
+    @NotNull(message = "Idade é obrigatória")
+    @Positive(message = "Idade deve ser positiva")
+    private Integer idade;
+
+    @NotBlank(message = "Rank é obrigatório")
     private String rank;
+
     private MissoesModel missoes;
 
 
