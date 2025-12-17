78a8f57bedb24e2ff8733b176aa570291a3d0693
Nadson Aguiar <164579532+NadsonAguiar@users.noreply.github.com>
2025-12-16 22:28:11 -0300

    feat: Cria um package e uma classe para tratar sobre as exceções criadas pelo MethodArgumentNotValidException, adiciona mensagem personalizada para identicar o erro exato da validação

--- src/main/java/dev/nadsonaguiar/CadastroDeNinjas/Exception/GlobalExceptionHandler.java
+++ src/main/java/dev/nadsonaguiar/CadastroDeNinjas/Exception/GlobalExceptionHandler.java
@@ -1,4 +1,36 @@
 package dev.nadsonaguiar.CadastroDeNinjas.Exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+// Classe criada para tratar exceptions
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    // Diz que esse metodo trata erro de validação
+    @ExceptionHandler
+    public ResponseEntity<?> exception(MethodArgumentNotValidException ex)
+    {
+
+        Map<String, String> erros = new HashMap<>();
+        // getBindingResult pega o relatório da validação
+        ex.getBindingResult()
+                // getFieldErrors = lista de campos com erro
+                .getFieldErrors()
+                // getField = nome do campo (nome, email)
+                // getDefaultMessage = mensagem do DTO
+                .forEach(error -> {
+                    erros.put(error.getField(), error.getDefaultMessage());
+                });
+        // badRequest = Status 400
+        return ResponseEntity.badRequest()
+                .body(erros);
+    }
 }
